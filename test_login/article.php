bad4111a49e01ae07e4eb4c03be6f051f13586eb
briyanyehezkhiel <114645700+briyanyehezkhiel@users.noreply.github.com>
2023-12-19 20:30:57 +0700

    Update article.php

--- test_login/article.php
+++ test_login/article.php
@@ -1,7 +1,7 @@
 <?php 
 
 session_start();
-
+ 
 if(!isset($_SESSION['ucode']) || (isset($_SESSION['ucode']) && empty($_SESSION['ucode']))){
     if(strstr($_SERVER['PHP_SELF'], 'login.php') === false)
     header('location:login.php');
